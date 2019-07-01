c14d0780cf3d123192e6cd254e67a695aafbfb47
Anil Paudel <anil59paudel@gmail.com>
2019-07-02 00:40:02 +0545

    new branch

--- api/product_details.js
+++ api/product_details.js
@@ -10,48 +10,47 @@ var Views= require('../models/product_views.model');
 //getting the product details //updating the rating of the product
  router.post('/update-rating',function(req,res,next){
     var userid= Auth.user_id;
-    
+
 })
 
 
 
 router.get('/:id',function(req,res){
     product_id= req.params.id;
-    
-    
+
+
     Product.findOne({"_id":product_id,"status":1}).populate('categories').populate('shop_id').populate('review.user_id').exec(function(err,data){
+      console.log("error is here");
         // var category = data.categories;
-        
+
         // var categories = new Category();
         // var category_data= [];
         // await Category.find({"_id":category},function(err,data){
-            
+
         //     category_data.push(data);
         //     });
-        
+
         // var shop =data.shop_id;
         // var shops = new User();
         // var shop_data = [];
         // await User.find({"_id":shop,"shopkeeper":1},function(err,data){
         //     shop_data.push(data);
-        // });      
+        // });
         if(err){
             response= {"error":true,"message":data}
         }else{
             // response={"error":false,"message":data,"category_list":category_data,"shop_details":shop_data}
             response={"error":false,"message":data}
         }
-        res.json(response);
-        
-        
-      
+        res.json(response);              
+
     });
-        
+
         });
 
  //updating the rating of the product
  router.post('/update-rating/:id',async function(req,res,next){
-    var userid="5ce29c9ca2eb5026b70aca12";    
+    var userid="5ce29c9ca2eb5026b70aca12";
     console.log(req.body);
     var productid= req.params.id;
     console.log(productid);
@@ -60,13 +59,13 @@ router.get('/:id',function(req,res){
         product.push(data);
         var shopid= product[0].shop_id;
         rating_userid=[]
-        
+
         rating_userid.push(product[0].rating);
-        
-        
-       
-    
-        
+
+
+
+
+
         if(err){
             console.log("error");
         }else{
@@ -76,11 +75,11 @@ router.get('/:id',function(req,res){
 
                 Product.updateOne({"_id":productid,"shop_id":shopid},
                 {
-                   
+
                     // $unset:{
                     //     userid:""
                     // },
-                   
+
 
                     $push:{
                         rating:{
@@ -88,24 +87,24 @@ router.get('/:id',function(req,res){
                             values:req.body.values
                         }
                     }
-                   
-                   
+
+
                 },{strict:false},
                 function(err,data){
                     if(err){
                         console.log(err);
                     }else{
-                        
+
                         res.json(data);
                     }
-                });  
+                });
         }
 
-    
-        
+
+
     });
 });
- 
+
 
     //updating the review of the product
     router.post('/update-review/:id',function(req,res,next){
@@ -115,7 +114,7 @@ router.get('/:id',function(req,res){
 
         Product.findOne({"_id":productid},function(err,data){
             product_details.push(data);
-            
+
             if(err){
                 res.json(err);
                 console.log(err);
@@ -144,7 +143,7 @@ router.get('/:id',function(req,res){
         }
         });
     });
- 
+
 
 
     //posting the click value of the product to count the views
@@ -158,18 +157,16 @@ router.get('/:id',function(req,res){
                     clickTime:click,
                 }
             }
-        },{upsert:true},function(err,data){
+        },{
+          upsert:true},function(err,data){
             if(err){
                 res.json(err)
-            }else{
+            }
+            else
+            {
                 res.json(data);
             }
         });
-        
     });
-    
-    
-
-
 
 module.exports= router;
